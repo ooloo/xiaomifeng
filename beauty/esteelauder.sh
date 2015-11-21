68327a05e5faa711ecc57691b76b3bf23b3a6177
root <root@xixiaya.com>
2015-11-21 19:05:50 +0800

    remove dir

--- beauty/esteelauder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget -r -I /templates/products/ -l 3 -A "multiproduct.tmpl*" \
-	-N -U "Mozilla/5.0(Windows; U; Windows NT 5.1; en-US)" --directory-prefix=/data/ \
-	--timeout=30 --wait=2 --random-wait http://www.esteelauder.com.cn/
